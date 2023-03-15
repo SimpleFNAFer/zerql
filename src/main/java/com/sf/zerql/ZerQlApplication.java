18fff8c240cfe5cc7e9fdd9fe3bc8b336a1f8dd0
Simple FNAFer <tnworks19@gmail.com>
2023-03-15 11:24:56 +0300

    init commit

---
+++ src/main/java/com/sf/zerql/ZerQlApplication.java
@@ -0,0 +1,13 @@
+package com.sf.zerql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sf/zerql/ZerQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sf.zerql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
